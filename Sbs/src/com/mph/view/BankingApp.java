f20ea09d1f10ab9fd1183e6b6a9972e58ae6595a
Gayathri A <97013869+Gayathri1602@users.noreply.github.com>
2022-01-04 16:44:06 +0530

    Update BankingApp.java
    added sop

--- Sbs/src/com/mph/view/BankingApp.java
+++ Sbs/src/com/mph/view/BankingApp.java
@@ -3,5 +3,6 @@ package com.mph.view;
 public class BankingApp {
 	public static void main(String[] args) {
 System.out.println("Welcome to Simple Banking management system");
+		System.out.println("Balance: " + balance); 
 }
 }
